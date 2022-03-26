7727081b4948ff24194029c5a6fc9ba0c0cee9d4
antoniosuciu <suciu.antonio.tudor@gmail.com>
2022-03-26 12:50:02 +0200

    Lab 4
    A HTML page with 2 buttons and 10 links. The first button changes the background, while the second button changes the shape of the links. The functionalities are implemented in JavaScript.

---
+++ University/JS/Lab 4/Lab4.js
@@ -0,0 +1,48 @@
+
+    let count1 = 0;
+    let count2 = 0;
+    let btn = document.getElementById("btn");
+    let btn2 = document.getElementById("btn2")
+
+
+    // var disp = document.getElementById("display");
+    // btn.onclick = function ()
+    // {
+    //     count1++;
+    //     // disp.innerHTML = count;
+    //     document.getElementById("myDIV").style.backgroundImage = `url(background${count1 % 5}.jpg)`;
+    // }
+
+    btn.onclick = function ()
+    {
+        const myInterval = setInterval(setImage, 3000);
+        setImage()
+    }
+
+    function setImage()
+    {
+        count1++;
+        document.getElementById("myDIV").style.backgroundImage = `url(background${count1 % 5}.jpg)`;
+    }
+
+    btn2.onclick = function ()
+    {
+        count2++;
+        let linkElements = document.getElementsByTagName('a');
+
+        for (let i = 0; i < linkElements.length; i++)
+        {
+            if(count2 % 2)
+            {
+                linkElements[i].style.color = 'navy';
+                linkElements[i].style.backgroundColor = 'orange';
+                linkElements[i].style.font='italic 15px arial,serif';
+            }
+            else
+            {
+                linkElements[i].style.color = 'orange';
+                linkElements[i].style.backgroundColor = 'navy';
+                linkElements[i].style.font='italic bold 20px arial,serif';
+            }
+        }
+    }
---
+++ University/JS/Lab 4/README.md
@@ -0,0 +1,10 @@
+#Lab 4 - JavaScript
+### Write an HTML document which contains
+- 2 buttons 
+- at least 10 links
+- a javascript
+
+When the user clicks **the first button**, the page's background will change 
+_(there are 5 background images which are rotated, one at a time)._
+
+When the user clicks **the second button**, the shape and color of all the links from the document will change.
